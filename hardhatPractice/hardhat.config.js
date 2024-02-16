bc8c80336583c3714460109852e2c3eb68dd6617
Yushiroh <Yushiroh.13@gmail.com>
2024-02-16 09:40:11 +0800

    hardhat configuration finished

--- hardhatPractice/hardhat.config.js
+++ hardhatPractice/hardhat.config.js
@@ -3,4 +3,26 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+
+  networks:{
+    arbsep:{
+      url:"",
+      accounts:[],
+    }
+  },
+  etherscan:{
+    apiKey:{
+      arbsep: ""
+    },
+    customChains:[
+      {
+        network: "arbsep",
+        chainId: 421614,
+        urls:{
+          apiURL: "",
+          browserURL: ""
+        }
+      }
+    ]
+  }
 };
